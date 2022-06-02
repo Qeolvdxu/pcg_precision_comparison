aee3dc658961c21d76b77b5e63b35ec97bce2a12
Hayden Estes <hayden1@br012.ib.bridges2.psc.edu>
2022-06-02 18:01:43 -0400

    input file now works and replaces hardcoded name

--- pcg_bfloat_comparison.m
+++ pcg_bfloat_comparison.m
@@ -1,7 +1,6 @@
+% TEST custom PCG algorithm with IEEE vs BFloat data types
 pkg load tablicious
 
-% TEST custom PCG algorithm with IEEE vs BFloat data types
-test_count = 20;
 max_iters = 20000;
 tol = 1e-6
 
@@ -20,12 +19,7 @@ name_cell{8,1}="RCM Default IC";
 name_cell{9,1}="RCM Bfloat16 IC";
 
 matrices = dir('test_subjects/*.mtx')
-
-% Sparse Matrix files from Suitesparse Collection
-matrices = {'arc130.mtx','494_bus.mtx','662_bus.mtx','685_bus.mtx','1138_bus.mtx'...
-            ,'bcsstk01.mtx','bcsstk02.mtx','bcsstk03.mtx','bcsstk04.mtx','bcsstk05.mtx'...
-            ,'bcsstk06.mtx','bcsstk07.mtx','bcsstk08.mtx','bcsstk09.mtx','bcsstk10.mtx'...
-            ,'bcsstk11.mtx','bcsstk12.mtx','bcsstk13.mtx','bcsstk14.mtx','bcsstk15.mtx'};
+test_count = size(matrices,1)
 
 % counters
 it_total1 = 1;
@@ -38,42 +32,42 @@ fprintf("Tests done:\n")
 for cur_test=1:test_count
 
     % Read matrix files
-    cur_matrix = matrices{cur_test};
-    [A, ~, A_size, nonzero_count] = mmread(cur_matrix);
-    
-    %x = randn(size(A,2), 1) * max(A);
-    %b = A * x;
-    %b = randn(size(A,1), 1);
-    b = ones(size(A,1),1);
+     cur_matrix = matrices(cur_test).name;
+     [A, ~, A_size, nonzero_count] = mmread(strcat('test_subjects/',cur_matrix));
 
-    %Standard Ordering
-    % calculate iteration count using standard floats
-    [~, ~, ~, itcount1] = custom_pcg(A, b, tol, max_iters, eye(A_size), 0);
+     %x = randn(size(A,2), 1) * max(A);
+     %b = A * x;
+     %b = randn(size(A,1), 1);
+     b = ones(size(A,1),1);
+
+     %Standard Ordering
+     % calculate iteration count using standard floats
+     [~, ~, ~, itcount1] = custom_pcg(A, b, tol, max_iters, eye(A_size), 0);
      % calculate iteration count using brain floats
-    [~, ~, ~, itcount2] = custom_pcg_bfloat16(A, b, tol, max_iters, eye(A_size), 0);
+     [~, ~, ~, itcount2] = custom_pcg_bfloat16(A, b, tol, max_iters, eye(A_size), 0);
 
-    perm = symrcm(A);
-    A = A(perm,perm);
-    
-    %RMC Ordering
-    % calculate iteration count using standard floats
-    [~, ~, ~, itcount3] = custom_pcg(A, b, tol, max_iters, eye(A_size), 0);
+     perm = symrcm(A);
+     A = A(perm,perm);
+
+     %RMC Ordering
+     % calculate iteration count using standard floats
+     [~, ~, ~, itcount3] = custom_pcg(A, b, tol, max_iters, eye(A_size), 0);
      % calculate iteration count using brain floats
-    [~, ~, ~, itcount4] = custom_pcg_bfloat16(A, b, tol, max_iters, eye(A_size), 0);
+     [~, ~, ~, itcount4] = custom_pcg_bfloat16(A, b, tol, max_iters, eye(A_size), 0);
 
-    % add data to table, show progress, get mean
-    table_cell{cur_test,1} = cur_test;
-    table_cell{cur_test,2} = cur_matrix;
-    table_cell{cur_test,3} = A_size;
-    table_cell{cur_test,4} = A_size/nonzero_count;
-    table_cell{cur_test,5} = cond(A);
-    table_cell{cur_test,6} = itcount1;
-    table_cell{cur_test,7} = itcount2;
-    table_cell{cur_test,8} = itcount3;
-    table_cell{cur_test,9} = itcount4;
+     % add data to table, show progress, get mean
+     table_cell{cur_test,1} = cur_test;
+     table_cell{cur_test,2} = cur_matrix;
+     table_cell{cur_test,3} = A_size;
+     table_cell{cur_test,4} = A_size/nonzero_count;
+     table_cell{cur_test,5} = cond(A);
+     table_cell{cur_test,6} = itcount1;
+     table_cell{cur_test,7} = itcount2;
+     table_cell{cur_test,8} = itcount3;
+     table_cell{cur_test,9} = itcount4;
 
-    fprintf("%3d: %s, %3d, %3d, %3d, %3d, %3d, %3d, %3d\n",cur_test,cur_matrix,A_size,A_size/nonzero_count,cond(A),itcount1,itcount2,itcount3,itcount4)
-end
+     fprintf("%3d: %s, %3d, %3d, %3d, %3d, %3d, %3d, %3d\n",cur_test,cur_matrix,A_size,A_size/nonzero_count,cond(A),itcount1,itcount2,itcount3,itcount4)
+     end
 
 % Create and print table
 table = cell2table(table_cell,'VariableNames',name_cell);
