384a727d49461d54bffe0c92bd729ad27ddbd365
qeolvdxu <qeolvdxu@pop-os.localdomain>
2022-08-02 17:37:16 -0500

    pcg development continues

--- C/pcg_precision_comparison.c
+++ C/pcg_precision_comparison.c
@@ -5,6 +5,7 @@
 #define PRECI_DT double
 #define PRECI_S "%lf "
 
+
 typedef struct my_csr_matrix {
   int n;
   int m;
@@ -14,15 +15,87 @@ typedef struct my_csr_matrix {
   int *rowptr;
 } my_csr_matrix;
 
-/* static double norm(int n, double *v) {
-   double norm;
+static double dotprod(int n, double *v, double *u) {
+  double x;
   int i;
 
-  for (i=0, norm=0.0; i<n; i++) norm += v[i]*v[i];
+  for (i = 0, x = 0.0; i < n; i++)
+    x += v[i] * u[i];
+
+  return x;
+}
+
+static double norm(int n, double *v) {
+
+  double norm;
+  int i;
+
+  for (i = 0, norm = 0.0; i < n; i++)
+    norm += v[i] * v[i];
 
   norm = sqrt(norm);
   return norm;
-  }*/
+}
+
+double *crsprod(my_csr_matrix *M, double *v) {
+  int i, j, k;
+  double *ans = malloc(sizeof(double) * M->n);
+  for (i = 0; i < M->n; i++)
+    ans[i] = 0;
+
+  int row = 0;
+  for (i = 0; i < M->nz; i++) {
+
+    if (i == M->rowptr[row])
+      row++;
+    ans[row - 1] += M->val[i] * v[M->col[i]];
+    printf("ans %d += %lf * %lf]\n", row - 1, M->val[i], v[M->col[i]]);
+  }
+  return ans;
+}
+
+int crs_cg(my_csr_matrix *M, PRECI_DT *b, double tol, int maxit) {
+  int i = 0;
+
+  double *x = malloc(sizeof(double) * M->n);
+  for (i = 0; i < M->n; i++)
+    x[i] = 0;
+  double alpha, beta;
+  double *p = malloc(sizeof(double) * M->n);
+  double *r = malloc(sizeof(double) * M->n);
+  double *q = malloc(sizeof(double) * M->n);
+  double *z = malloc(sizeof(double) * M->n);
+  int v;
+  // r = b - M * x;
+  r = crsprod(M, x);
+  for (i = 0; i < M->n; i++) {
+    printf("%lf, ", r[i]);
+    r[i] = b[i] - r[i];
+  }
+  z = r;
+  p = z;
+  i = 0;
+  while (i <= maxit && norm(M->n, r) / norm(M->n, b) > tol) {
+    printf("i:%d\nnorm_r: %lf norm_b: %lf\n\n", i, norm(M->n, r),
+           norm(M->n, b));
+    q = crsprod(M, p);
+    v = dotprod(M->n, r, z);
+
+    // alpha =  / dot(p,q)
+    alpha = v / dotprod(M->n, p, q);
+
+    // x = x + alpha*p
+
+    // r = r - alpha*q
+
+    beta = dotprod(M->n, r, z) / v;
+
+    // p = z + beta * p;
+
+    i++;
+  }
+     return 0;
+}
 
 my_csr_matrix *csrread(char *name) {
   struct my_csr_matrix *M = malloc(sizeof(struct my_csr_matrix));
@@ -31,6 +104,7 @@ my_csr_matrix *csrread(char *name) {
 
   fscanf(file, "%d %d %d", &M->m, &M->n, &M->nz);
   M->val = malloc(sizeof(PRECI_DT) * M->nz);
+
   M->col = malloc(sizeof(int) * M->nz);
   M->rowptr = malloc(sizeof(int) * M->n);
 
@@ -45,40 +119,56 @@ my_csr_matrix *csrread(char *name) {
   return M;
 }
 
-  void free_matrix(my_csr_matrix * M) {
-    free(M->val);
-    free(M->col);
-    free(M->rowptr);
-    free(M);
+void free_matrix(my_csr_matrix * M) {
+  free(M->val);
+  free(M->col);
+  free(M->rowptr);
+  free(M);
 
-    return;
+  return;
+}
+
+void print_csr(my_csr_matrix * M) {
+  int i = 0;
+
+  printf("rowptr,");
+  for (i = 0; i < M->n; i++)
+    printf("%d, ", M->rowptr[i]);
+  printf("\n\n");
+
+  printf("index,");
+  for (i = 0; i < M->nz; i++)
+    printf("%d, ", M->col[i]);
+  printf("\n\n");
+
+  printf("values,");
+  for (i = 0; i < M->nz; i++) {
+    printf(PRECI_S, M->val[i]);
+    printf(", ");
   }
+  printf("\n\n");
+}
 
-  void print_csr(my_csr_matrix *M) {
-    int i = 0;
+int main(void) {
+  int i;
 
-    printf("rowptr,");
-    for (i = 0; i < M->n; i++)
-      printf("%d, ", M->rowptr[i]);
-    printf("\n\n");
+  struct my_csr_matrix *test = csrread("./test_subjects/bob.mtx.crs");
+  print_csr(test);
+  double *v = malloc(sizeof(double) * test->n);
+  double *a = malloc(sizeof(double) * test->n);
+  double *b = malloc(sizeof(double) * test->n);
+  for (i = 0; i < test->n; i++)
 
-    printf("index,");
-    for (i = 0; i < M->nz; i++)
-      printf("%d, ", M->col[i]);
-    printf("\n\n");
+    v[0] = 10;
 
-    printf("values,");
-    for (i = 0; i < M->nz; i++)
-      printf("%lf, ", M->val[i]);
-    printf("\n\n");
+  v[1] = 11;
 
+  v[2] = 12;
 
-  }
-  int main(void) {
+  for (i = 0; i < test->n; i++)
+    b[i] = 1;
 
-    struct my_csr_matrix *test = csrread("./test_subjects/bcsstk04.mtx.crs");
-    print_csr(test);
-    free_matrix(test);
-
-    return 0;
-  }
+  crs_cg(test, b, 1e-6, 8000);
+  free_matrix(test);
+  return 0;
+}
