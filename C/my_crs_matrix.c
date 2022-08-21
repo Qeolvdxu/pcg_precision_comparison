074ea294c4d5b1fa1939529fbeac870833148312
qeolvdxu <qeolvdxu@pop-os.localdomain>
2022-08-20 21:25:48 -0500

    no more segfault

--- C/my_crs_matrix.c
+++ C/my_crs_matrix.c
@@ -28,21 +28,21 @@ static double norm(int n, double *v) {
 }
 
 // multiply a my_crs_matrix with a vector
-double *my_crs_times_vec(my_crs_matrix *M, double *v) {
-  int i;
-  double *ans = malloc(sizeof(double) * M->n);
+int my_crs_times_vec(my_crs_matrix *M, double *v, double *ans) {
+  int i, j;
   for (i = 0; i < M->n; i++)
     ans[i] = 0;
 
-  int row = 0;
-  for (i = 0; i < M->nz; i++) {
-
-    if (i == M->rowptr[row])
-      row++;
-    ans[row - 1] += M->val[i] * v[M->col[i]];
-    printf("ans %d += %lf * %lf]\n", row - 1, M->val[i], v[M->col[i]]);
+  for (i = 0; i < M->n; i++) {
+    for (j = M->rowptr[i]; j < M->rowptr[i + 1]; j++) {
+      ans[i] = ans[i] + M->val[j] * v[M->col[j]];
+    }
+    // printf("%lf ", ans[i]);
   }
-  return ans;
+  /* if (i == M->rowptr[row]) row++;
+             ans[row - 1] += M->val[i] * v[M->col[i]];
+	     }*/
+  return 0;
 }
 
 double *my_crs_cg(my_crs_matrix *M, PRECI_DT *b, double tol, int maxit) {
@@ -53,27 +53,28 @@ double *my_crs_cg(my_crs_matrix *M, PRECI_DT *b, double tol, int maxit) {
     x[i] = 0;
   double alpha, beta;
   double *p = malloc(sizeof(double) * M->n);
-  double *r; // = malloc(sizeof(double) * M->n);
-  double *q; // = malloc(sizeof(double) * M->n);
+  double *r       = malloc(sizeof(double) * M->n);
+  double *q  = malloc(sizeof(double) * M->n);
   double *z = malloc(sizeof(double) * M->n);
 
   // Set up to iterate
-  r = my_crs_times_vec(M, x);
+  my_crs_times_vec(M, x, r);
   for (i = 0; i < M->n; i++) {
-    printf("%lf, ", r[i]);
+    // printf("%lf, ", r[i]);
     r[i] = b[i] - r[i];
   }
   for (i = 0; i < M->n; i++)
     z[i] = r[i];
+
   for (i = 0; i < M->n; i++)
     p[i] = z[i];
 
   // Start iteration
   i = 0;
   while (i <= maxit && norm(M->n, r) / norm(M->n, b) > tol) {
-    printf("\n\ni:%d\nnorm_r: %lf norm_b: %lf\n", i, norm(M->n, r),
-           norm(M->n, b));
-    q = my_crs_times_vec(M, p);
+    // printf("\n\ni:%d\nnorm_r: %lf norm_b: %lf\n", i, norm(M->n, r),
+    // norm(M->n, b));
+    my_crs_times_vec(M, p, q);
     v = dotprod(M->n, r, z);
 
     // alpha =  / dot(p,q)
@@ -87,23 +88,51 @@ double *my_crs_cg(my_crs_matrix *M, PRECI_DT *b, double tol, int maxit) {
     // r = r - alpha*q
     for (j = 0; j < M->n; j++)
       r[j] -= alpha * q[j];
+
+    for (j = 0; j < M->n; j++)
+      z[j] = r[j];
+
     beta = dotprod(M->n, r, z) / v;
 
     // p = z + beta * p;
     for (j = 0; j < M->n; j++) {
       p[j] = z[j] + beta * p[j];
-      printf("%lf %lf %lf\n", p[j], z[j], beta * p[j]);
-    }
-    free(q);
-    i++;
-  }
+      }
+    /* printf("%lf %lf %lf\n", p[j], z[j], beta * p[j]);
+	 }
 
+      printf("\n p vector: ");
+      for (j = 0; j < M->n; j++)
+        printf("%lf, ", p[j]);
+      printf("%lf, ", p[j]);
+
+      printf("\n r vector: ");
+      for (j = 0; j < M->n; j++)
+        printf("%lf, ", r[j]);
+      printf("%lf, ", r[j]);
+
+      printf("\n q vector: ");
+      for (j = 0; j < M->n; j++)
+        printf("%lf, ", q[j]);
+      printf("%lf, ", q[j]);
+
+      printf("\n z vector: ");
+      for (j = 0; j < M->n; j++)
+        printf("%lf, ", z[j]);
+      printf("%lf, ", z[j]);
+
+      printf("\n alpha: %lf ", alpha);
+      printf("\n beta: %lf ", beta);*/
+
+      i++;
+  }
+  printf("\n *total of %d iterations* \n", i);
   free(p);
-  // free(q);
+  free(q);
   free(z);
   free(r);
-
-  return x;
+  free(x);
+       return 0;
 }
     // read matrix file into a my_csr_matrix variable
 my_crs_matrix *my_crs_read(char *name) {
--- C/pcg_precision_comparison.c
+++ C/pcg_precision_comparison.c
@@ -5,27 +5,49 @@
 #include "my_crs_matrix.h"
 
 int main(void) {
-  int i;
-  my_crs_matrix *test = my_crs_read("./test_subjects/test.mtx.crs");
-  double *b = malloc(sizeof(double) * test->n);
-  double *ans;
-  my_crs_print(test);
+  int i, tests;
+  int test_count = 18;
+  my_crs_matrix *test;
+  double* b;
 
-  // b vector of 1s
+    char *files[18] = {
+      "./test_subjects/494_bus.mtx.crs",
+      "./test_subjects/662_bus.mtx.crs",
+      //"./test_subjects/685_bus.mtx.crs",
+      "./test_subjects/dwt_869.mtx.crs",
+
+      "./test_subjects/bcsstk01.mtx.crs",
+      "./test_subjects/bcsstk02.mtx.crs",
+      "./test_subjects/bcsstk03.mtx.crs",
+      "./test_subjects/bcsstk04.mtx.crs",
+      "./test_subjects/bcsstk05.mtx.crs",
+      "./test_subjects/bcsstk06.mtx.crs",
+      "./test_subjects/bcsstk07.mtx.crs",
+      "./test_subjects/bcsstk08.mtx.crs",
+      "./test_subjects/bcsstk09.mtx.crs",
+      "./test_subjects/bcsstk10.mtx.crs",
+      "./test_subjects/bcsstk11.mtx.crs",
+      "./test_subjects/bcsstk12.mtx.crs",
+      "./test_subjects/bcsstk13.mtx.crs",
+      "./test_subjects/bcsstk14.mtx.crs"
+    };
+for (tests=0; tests<test_count;tests++)
+      {
+	printf("\n%s",files[tests]);
+
+	test = my_crs_read(files[tests]);//"./test_subjects/bcsstk10.mtx.crs");
+    b = malloc(sizeof(double) * test->n);
+
+
+    // b vector of 1s
   for (i = 0; i < test->n; i++)
     b[i] = 1;
 
   // apply CG
-  ans = my_crs_cg(test, b, 1e-6, 8000);
-  printf("\nans = ");
-  for (i = 0; i < test->n; i++)
-    printf("%lf, ", ans[i]);
-  printf("\n");
-
-  
+  my_crs_cg(test, b, 1e-6, 80000);
+  }
   // free
   my_crs_free(test);
   free(b);
-  free(ans);
   return 0;
 }
