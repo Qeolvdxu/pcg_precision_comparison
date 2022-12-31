207804417cfc719d5aa2b55ea2af0f05ba451c7b
Hayden <57072675+Qeolvdxu@users.noreply.github.com>
2022-12-31 11:01:56 -0600

    Update CUDA_cg.cu

--- CUDA_cg.cu
+++ CUDA_cg.cu
@@ -3,10 +3,10 @@
 #include <math.h>
 #include <time.h>
 
+#include "my_crs_matrix.h"
+
 #include <cuda_runtime.h>
 #include <cusparse.h>
-
-#define PRECI_DT float 
 
 // find the dot product of two vectors
 __device__ static PRECI_DT dotprod(int n, PRECI_DT *v, PRECI_DT *u) {
@@ -147,6 +147,31 @@ __global__ void cgkernel(int n, int m, int nz,
 
 }
 
+__host__ my_crs_2_cusparse(my_crs_matrix* A, cusparseHandle_t cusparseHandle);
+{
+	int nnz = A->nz;
+	int n = A->n;
+
+	// Allocate memory for the CSR matrix
+	int *ptr, *indices, *data;
+	cudaMalloc((void **)&ptr, (n+1) * sizeof(int));
+	cudaMalloc((void **)&indices, nnz * sizeof(int));
+	cudaMalloc((void **)&data, nnz * sizeof(PRECI_DT));
+
+	// Copy data from host to device
+	cudaMemcpy(ptr, A->rowptr, (n+1) * sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(indices, A->col, nnz * sizeof(int), cudaMemcpyHostToDevice);
+	cudaMemcpy(data, A->val, nnz * sizeof(PRECI_DT), cudaMemcpyHostToDevice);
+
+	// Create the CSR matrix
+	cusparseMatDescr_t descr;
+	cusparseCreateMatDescr(&descr);
+	cusparseSetMatType(descr, CUSPARSE_MATRIX_TYPE_GENERAL);
+	cusparseSetMatIndexBase(descr, CUSPARSE_INDEX_BASE_ZERO);
+	cusparseHybMat_t csrMat;
+	cusparseCreateHybMat(&csrMat);
+	cusparseDcsr2hyb(cusparseHandle, n, n, descr, data, ptr, indices, csrMat, 0, CUSPARSE_HYB_PARTITION_AUTO);
+}
 
 int main(void)
 {
