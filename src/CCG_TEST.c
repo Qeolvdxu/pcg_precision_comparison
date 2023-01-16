d244f96d7b97c322eb2f02a8ebd2370d433280d9
Hayden <hve0002@uah.edu>
2023-01-15 21:00:25 -0600

    file path update

--- src/CCG_TEST.c
+++ src/CCG_TEST.c
@@ -8,7 +8,7 @@
 
 int main(int argc, char* argv[]) {
   int i, tests;
-  int test_count = 1;
+  int test_count = 17;
   my_crs_matrix *test;
   my_crs_matrix *test_RCM;
   my_crs_matrix *precond;
@@ -18,25 +18,24 @@ int main(int argc, char* argv[]) {
       printf("ERROR: command line arguments invalid/missing\n");
       return 1;
     }
-  char *files[18] = {
-    "./test_subjects/494_bus.mtx.crs",
-    "./test_subjects/662_bus.mtx.crs",
-    "./test_subjects/685_bus.mtx.crs",
-    "./test_subjects/dwt_869.mtx.crs",
-    "./test_subjects/bcsstk01.mtx.crs",
-    "./test_subjects/bcsstk02.mtx.crs",
-    "./test_subjects/bcsstk03.mtx.crs",
-    "./test_subjects/bcsstk04.mtx.crs",
-    "./test_subjects/bcsstk05.mtx.crs",
-    "./test_subjects/bcsstk06.mtx.crs",
-    "./test_subjects/bcsstk07.mtx.crs",
-    "./test_subjects/bcsstk08.mtx.crs",
-    "./test_subjects/bcsstk09.mtx.crs",
-    "./test_subjects/bcsstk10.mtx.crs",
-    "./test_subjects/bcsstk11.mtx.crs",
-    "./test_subjects/bcsstk12.mtx.crs",
-    "./test_subjects/bcsstk13.mtx.crs",
-    "./test_subjects/bcsstk14.mtx.crs"
+  char *files[17] = {
+    "../test_subjects/bcsstk10.mtx.crs",
+    "../test_subjects/662_bus.mtx.crs",
+    "../test_subjects/685_bus.mtx.crs",
+    "../test_subjects/dwt_869.mtx.crs",
+    "../test_subjects/bcsstk01.mtx.crs",
+    "../test_subjects/bcsstk02.mtx.crs",
+    "../test_subjects/bcsstk03.mtx.crs",
+    "../test_subjects/bcsstk04.mtx.crs",
+    "../test_subjects/bcsstk05.mtx.crs",
+    "../test_subjects/bcsstk06.mtx.crs",
+    "../test_subjects/bcsstk07.mtx.crs",
+    "../test_subjects/bcsstk08.mtx.crs",
+    "../test_subjects/bcsstk09.mtx.crs",
+    "../test_subjects/bcsstk11.mtx.crs",
+    "../test_subjects/bcsstk12.mtx.crs",
+    "../test_subjects/bcsstk13.mtx.crs",
+    "../test_subjects/bcsstk14.mtx.crs"
   };
   PRECI_DT tol = (float)atof(argv[2]);
 
@@ -47,7 +46,7 @@ int main(int argc, char* argv[]) {
       printf("\n%s\n",files[tests]);
 
       maxit = atoi(argv[1])-1;
-      test = my_crs_read(files[tests]);//"../test_subjects/bcsstk10.mtx.crs");
+      test = my_crs_read("../test_subjects/bcsstk10.mtx.crs");
       test_RCM = rcm_reorder(test);
       precond = eye(test->n);
       PRECI_DT* b;
