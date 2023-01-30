ed3c93cff5f265fbb5b6e15c8f3f451853731bd7
qeolvdxu <hve0002@uah.edu>
2023-01-29 20:26:07 -0600

    read straight into cusparse

--- src/CuCG.cu
+++ src/CuCG.cu
@@ -12,141 +12,128 @@
 #define PRECI_S "%lf "
 #define PRECI_CUDA CUDA_R_64F
 
-// compressed sparse row matrix
-typedef struct {
-  int n;
-  int m;
-  int nz;
-  PRECI_DT *val;
-  int *col;
-  int *rowptr;
-} my_crs_matrix;
-
-
-__global__ int cgkernel(cusparseSpMatDescr_t A,
-			cusparseSpMatDescr_t M,
+__global__ void cgkernel(cusparseSpMatDescr_t A,
+			 cusparseSpMatDescr_t M,
 			PRECI_DT *b,
 			PRECI_DT *x,
 			int max_iter,
 			PRECI_DT tolerance)
-
 {
-
-  return max_iter;
+  printf("Hello cuda!");
+  return;
 }
 
 
-//cusparseSpMatDescr_t device_A = my_crs_2_cusparse(host_A, device_A, cusparseHandle);
 
-__host__ cusparseSpMatDescr_t my_crs_2_cusparse(my_crs_matrix* A)
+__host__ cusparseSpMatDescr_t cusparse_crs_read(char* name)
 {
   cusparseSpMatDescr_t desc;
-  int nnz = A->nz;
-  int n = A->n;
+  PRECI_DT* val;
+  int* col;
+  int* rowptr;
 
-  // Allocate memory for the CSR matrix
-  int *ptr, *indices;
-  PRECI_DT* data;
-  cudaMalloc((void **)&ptr, (n+1) * sizeof(int));
-  cudaMalloc((void **)&indices, nnz * sizeof(int));
-  cudaMalloc((void **)&data, nnz * sizeof(PRECI_DT));
-
-  // Copy data from host to device
-  cudaMemcpy(ptr, A->rowptr, (n+1) * sizeof(int), cudaMemcpyHostToDevice);
-  cudaMemcpy(indices, A->col, nnz * sizeof(int), cudaMemcpyHostToDevice);
-  cudaMemcpy(data, A->val, nnz * sizeof(PRECI_DT), cudaMemcpyHostToDevice);
-
-  //Create HHthe CSR matrix
-  cusparseCreateCsr(&desc, n, n, nnz, A->rowptr, A->col, A->val, CUSPARSE_INDEX_32I, CUSPARSE_INDEX_32I, CUSPARSE_INDEX_BASE_ZERO, PRECI_CUDA);
-  return desc;
-}
-
-    __host__ void my_crs_free(my_crs_matrix *M) {
-    free(M->val);
-    free(M->col);
-    free(M->rowptr);
-    free(M);
-
-    return;
-}
-
-__host__ my_crs_matrix *my_crs_read(char *name) {
-  printf("test 1\n");
-
-  my_crs_matrix *M = (my_crs_matrix*)malloc(sizeof(my_crs_matrix));
-  printf("test 2\n");
-
-
+  int n = 0;
+  int m = 0;
+  int nz = 0;
   FILE *file;
   if ((file = fopen(name, "r"))) {
     int i;
 
-    fscanf(file, "%d %d %d", &M->m, &M->n, &M->nz);
-    M->val = (PRECI_DT*)malloc(sizeof(PRECI_DT) * M->nz);
-    M->col = (int*)malloc(sizeof(int) * M->nz);
-    M->rowptr = (int*)malloc(sizeof(int) * M->n);
+    fscanf(file, "%d %d %d", &m, &n, &nz);
 
-    for (i = 0; i <= M->n; i++)
-      fscanf(file, "%d ", &M->rowptr[i]);
-    for (i = 0; i < M->nz; i++)
-      fscanf(file, "%d ", &M->col[i]);
-    for (i = 0; i < M->nz; i++)
-      fscanf(file, PRECI_S, &M->val[i]);
+    /*PRECI_DT* val = new PRECI_DT[nz];
+      int* col = new int[nz];
+    int* rowptr = new int[n];*/
 
-    fclose(file);
+     val = (PRECI_DT*)malloc(sizeof(PRECI_DT)*nz);
+     col = (int*)malloc(sizeof(int)*nz);
+     rowptr = (int*)malloc(sizeof(int)*n+1);
+    
+
+     for (i = 0; i <= n; i++)
+       fscanf(file, "%d ", &rowptr[i]);
+
+     for (i = 0; i < nz; i++)
+       fscanf(file, "%d ", &col[i]);
+       for (i = 0; i < nz; i++)
+    fscanf(file, PRECI_S, &val[i]);
+
+
+
+       fclose(file);
+
+
+       // Allocate memory for the CSR matrix
+      int *ptr, *indices;
+      PRECI_DT* data;
+      cudaMalloc((void **)&ptr, (n+1) * sizeof(int));
+      cudaMalloc((void **)&indices, nz * sizeof(int));
+      cudaMalloc((void **)&data, nz * sizeof(PRECI_DT));
+
+      // Copy data from host to device
+      cudaMemcpy(ptr, rowptr, (n+1) * sizeof(int), cudaMemcpyHostToDevice);
+      cudaMemcpy(indices, col, nz * sizeof(int), cudaMemcpyHostToDevice);
+      cudaMemcpy(data, val, nz * sizeof(PRECI_DT), cudaMemcpyHostToDevice);
+
+      //Create HHthe CSR matrix
+      cusparseCreateCsr(&desc, n, n, nz, rowptr, col, val, CUSPARSE_INDEX_32I, CUSPARSE_INDEX_32I, CUSPARSE_INDEX_BASE_ZERO, PRECI_CUDA);
+
   } else {
     printf("ERROR: could not open file %s\n", name);
-    M->n = -1;
+    n = -1;
   }
-  printf("test 3\n");
-
-
-  return M;
+  return desc;
 }
+
 
 extern "C" void call_CuCG(void)
 {
+  printf("Creating cusparse handle!\n");
   cusparseHandle_t cusparseHandle;
   cusparseStatus_t status = cusparseCreate(&cusparseHandle);
   if (status != CUSPARSE_STATUS_SUCCESS)
     {
-      printf("Error creating cusparse Handle!"); 
+      printf("Error creating cusparse Handle!\n"); 
     }
-  else
-    {
-      my_crs_matrix *host_A;
-      host_A = my_crs_read((char*)"../test_subjects/rcm/bcsstk10.mtx.rcm.csr");
+     else
+       {
+	 printf("reading matrix file...\n");
+	 cusparseSpMatDescr_t device_A = cusparse_crs_read((char*)"../test_subjects/rcm/bcsstk10.mtx.rcm.csr");
 
-      PRECI_DT* h_x = new PRECI_DT[host_A->n];
-      PRECI_DT* h_b = new PRECI_DT[host_A->n];
+	 int n=10;
+	 int nz = 20;
 
-      for(int i=0;i<host_A->n;i++)
-	{
-	  h_x[i] = 1;
-	  h_b[i] = 0;
-	}
-  
+	 printf("allocating host vectors...\n");
+	 PRECI_DT* h_x = new PRECI_DT[n];
+	 PRECI_DT* h_b = new PRECI_DT[n];
 
-      PRECI_DT* d_x;
+	 printf("setting host vector values...\n");
+	 for(int i=0;i<n-1;i++)
+	   {
+	     h_x[i] = 1;
+	     h_b[i] = 0;
+	   }
+	     printf("Created Host Vectors!\n");  
+
+		    PRECI_DT* d_x;
       PRECI_DT* d_b;
 
-      cudaMalloc(&d_x, host_A->n * sizeof(PRECI_DT));
-      cudaMalloc(&d_b, host_A->n * sizeof(PRECI_DT));
+      cudaMalloc(&d_x, n * sizeof(PRECI_DT));
+      cudaMalloc(&d_b, n * sizeof(PRECI_DT));
 
-      cudaMemcpy(d_x, h_x, host_A->n * sizeof(PRECI_DT), cudaMemcpyHostToDevice);
-      cudaMemcpy(d_b, h_b, host_A->n * sizeof(PRECI_DT), cudaMemcpyHostToDevice);
-
-      cusparseSpMatDescr_t device_A = my_crs_2_cusparse(host_A);
-      //cgkernel<<<1,(host_A->n*8)+host_A->nz>>>(device_A, device_A, d_b,d_x,8000,1e-7);
+      cudaMemcpy(d_x, h_x, n * sizeof(PRECI_DT), cudaMemcpyHostToDevice);
+      cudaMemcpy(d_b, h_b, n * sizeof(PRECI_DT), cudaMemcpyHostToDevice);
 
 
-      my_crs_free(host_A);
-    }
-  cusparseDestroy(cusparseHandle);
+
+      printf("Calling cgkernel...\n");
+      cgkernel<<<1,(n*8)+nz>>>(device_A, device_A, d_b,d_x,8000,1e-7);
+      cusparseDestroy(cusparseHandle);
+       }
   printf("Done!\n");
     
     
-  return;
+    return;
 }
 
 int main (void)
