6a82cfaa36237f761615a505bdb7e17bd33625a4
Hayden <hve0002@uah.edu>
2023-05-13 18:44:07 -0700

    fixed read error of size 4 in rowptr scanf

--- src/my_crs_matrix.c
+++ src/my_crs_matrix.c
@@ -15,16 +15,15 @@ my_crs_matrix *sparse_transpose(my_crs_matrix *input) {
 
   res->val = malloc(sizeof(PRECI_DT) * res->nz);
 
-  res->col = malloc(sizeof(int) *res->nz);
+  res->col = malloc(sizeof(int) * res->nz);
   res->rowptr = malloc(sizeof(int) * (res->n + 2));
 
-  for (i = 0; i < res->n+2; i++)
+  for (i = 0; i < res->n + 2; i++)
     res->rowptr[i] = 0;
   for (i = 0; i < res->nz; i++)
     res->col[i] = 0;
   for (i = 0; i < res->nz; i++)
     res->val[i] = 0;
-
 
   // count per column
   for (int i = 0; i < input->nz; ++i) {
@@ -32,7 +31,7 @@ my_crs_matrix *sparse_transpose(my_crs_matrix *input) {
   }
 
   // from count per column generate new rowPtr (but shifted)
-  for (int i = 2; i < res->n+2; ++i) {
+  for (int i = 2; i < res->n + 2; ++i) {
     // create incremental sum
     res->rowptr[i] += res->rowptr[i - 1];
   }
@@ -40,13 +39,15 @@ my_crs_matrix *sparse_transpose(my_crs_matrix *input) {
   // perform the main part
   for (int i = 0; i < input->n; ++i) {
     for (int j = input->rowptr[i]; j < input->rowptr[i + 1]; ++j) {
-      // calculate index to transposed matrix at which we should place current element, and at the same time build final rowPtr
+      // calculate index to transposed matrix at which we should place current
+      // element, and at the same time build final rowPtr
       const int new_index = res->rowptr[input->col[j] + 1]++;
       res->val[new_index] = input->val[j];
       res->col[new_index] = i;
     }
   }
-  //res->rowptr = realloc(res->rowptr,res->n*sizeof(PRECI_DT));; // pop that one extra
+  // res->rowptr = realloc(res->rowptr,res->n*sizeof(PRECI_DT));; // pop that
+  // one extra
 
   return res;
 }
@@ -62,7 +63,7 @@ my_crs_matrix *my_crs_read(char *name) {
     M->val = malloc(sizeof(PRECI_DT) * M->nz);
 
     M->col = malloc(sizeof(int) * M->nz);
-    M->rowptr = malloc(sizeof(int) * M->n+1);
+    M->rowptr = malloc(sizeof(int) * (M->n + 1));
 
     for (i = 0; i <= M->n; i++)
       fscanf(file, "%d ", &M->rowptr[i]);
@@ -71,19 +72,19 @@ my_crs_matrix *my_crs_read(char *name) {
     for (i = 0; i < M->nz; i++)
       fscanf(file, PRECI_S, &M->val[i]);
 
-  /*  printf("CCG rowptr: ");
-    for (i = 0; i <= M->n; i++)
-      printf("%d ", M->rowptr[i]);
-    printf("\n\n");
-    printf("CCG col: ");
-    for (i = 0; i < M->nz; i++)
-      printf("%d ", M->col[i]);
-    printf("\n\n");
-    printf("CCG val: ");
-    for (i = 0; i < M->nz; i++)
-      printf(PRECI_S, M->val[i]);
-    printf("\n\n");
-*/	
+    /*  printf("CCG rowptr: ");
+      for (i = 0; i <= M->n; i++)
+        printf("%d ", M->rowptr[i]);
+      printf("\n\n");
+      printf("CCG col: ");
+      for (i = 0; i < M->nz; i++)
+        printf("%d ", M->col[i]);
+      printf("\n\n");
+      printf("CCG val: ");
+      for (i = 0; i < M->nz; i++)
+        printf(PRECI_S, M->val[i]);
+      printf("\n\n");
+  */
 
     fclose(file);
   } else {
@@ -91,7 +92,7 @@ my_crs_matrix *my_crs_read(char *name) {
     M->n = -1;
   }
   return M;
-  }
+}
 
 // makes identity matrix in csr
 my_crs_matrix *eye(int n) {
@@ -114,10 +115,8 @@ my_crs_matrix *eye(int n) {
   for (i = 0; i < M->nz; i++)
     M->val[i] = 1;
 
-
   return M;
 }
-
 
 // Free my_csr_matrix variable
 void my_crs_free(my_crs_matrix *M) {
