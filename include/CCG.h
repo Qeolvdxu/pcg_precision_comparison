fb642390251919a2fabc358d4e4b53be44f71568
qeolvdxu <hve0002@uah.edu>
2023-05-23 17:45:03 -0500

    header updates to src changes

--- include/CCG.h
+++ include/CCG.h
@@ -2,15 +2,15 @@
 #define CCG_MATRIX_H_
 
 // the big one
-int CCG(my_crs_matrix *A, my_crs_matrix *M, PRECI_DT *b, PRECI_DT *x, int max_iter, PRECI_DT tolerance, int (*precond_fn)(void*,void* x,void* b), void* precond_args);
+void CCG(my_crs_matrix *A, my_crs_matrix *M, PRECI_DT *b, PRECI_DT *x,
+         int max_iter, PRECI_DT tolerance, int *iter, double *elapsed);
 // cg preconditioning related functions
-void ichol(my_crs_matrix* M, double* L);
-void precondition(my_crs_matrix* M, PRECI_DT* r, PRECI_DT* z);
+void ichol(my_crs_matrix *M, double *L);
+void precondition(my_crs_matrix *M, PRECI_DT *r, PRECI_DT *z, my_crs_matrix *L);
 
 // various linear algebra functions needed for cg
-PRECI_DT matvec_dot(my_crs_matrix *A, PRECI_DT* x, PRECI_DT* y, int n);
+PRECI_DT matvec_dot(my_crs_matrix *A, PRECI_DT *x, PRECI_DT *y, int n);
 PRECI_DT dot(PRECI_DT *v, PRECI_DT *u, int n);
-void matvec(my_crs_matrix *A, PRECI_DT* x, PRECI_DT* y);
+void matvec(my_crs_matrix *A, PRECI_DT *x, PRECI_DT *y);
 PRECI_DT norm(int n, PRECI_DT *v);
-
 #endif // CCG_MATRIX_H_
--- include/CuCG.h
+++ include/CuCG.h
@@ -3,6 +3,8 @@
 
 #define PRECI_DT double
 
-extern void call_CuCG(char* name, PRECI_DT* h_b, PRECI_DT* h_x, int maxit, PRECI_DT tol);
+extern void call_CuCG(char *name, char *m_name, PRECI_DT *h_b, PRECI_DT *h_x,
+                      int maxit, PRECI_DT tol, int *iter, double *elpased,
+                      double *mem_elapsed);
 
 #endif // CuCG_MATRIX_H_
