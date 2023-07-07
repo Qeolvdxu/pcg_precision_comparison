775ff83250b2379c0ad012ebcdbbca0eaf0acce5
qeolvdxu <hve0002@uah.edu>
2023-07-07 16:52:07 -0500

    removed dupe

--- include/scripts/sparse2csr.m
+++
@@ -1,7 +0,0 @@
-function [val, row_ptr, col_ind] = sparse2csr(A)
-    m = size(A, 1);
-    [col, row, val] = find(A.');
-    
-    col_ind = col - 1;
-    row_ptr = [0; cumsum(histc(row, 1:m))];
-end
